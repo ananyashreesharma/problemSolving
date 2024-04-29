ccbb5d7565bee7a6df469658a41d6358f338e09e
anannyashree sharma <64601048+ananyashreesharma@users.noreply.github.com>
2024-04-30 01:16:44 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (37.68%) - LeetSync

--- 119-pascals-triangle-ii/pascals-triangle-ii.cpp
+++ 119-pascals-triangle-ii/pascals-triangle-ii.cpp
@@ -1,14 +1,10 @@
 class Solution {
 public:
     vector<int> getRow(int n) {
-     vector<int> result(n + 1, 1); // Initialize the vector with n + 1 elements, all set to 1
-    
-    // Generate elements of the row using the previous row
-    for (int i = 1; i < n; i++) {
-        // Update each element in the row based on the previous row and symmetry property
-        result[i] = (long long) result[i - 1] * (n - i + 1) / i;
-    }
-    
-    return result;
+     vector<int> result(n+1,1);
+     for(int i=1;i<n;i++){
+         result[i]=(long long)result[i-1]*(n-i+1)/i;
+     }
+     return result;
     }
 };
