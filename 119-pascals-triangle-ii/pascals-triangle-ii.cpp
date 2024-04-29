0b6b57b34898218b29103a92631ee153b0aae4ab
anannyashree sharma <64601048+ananyashreesharma@users.noreply.github.com>
2024-04-30 01:39:51 +0530

    Time: 3 ms (38.53%) | Memory: 7.5 MB (88.03%) - LeetSync

--- 119-pascals-triangle-ii/pascals-triangle-ii.cpp
+++ 119-pascals-triangle-ii/pascals-triangle-ii.cpp
@@ -1,10 +1,16 @@
 class Solution {
 public:
-    vector<int> getRow(int n) {
-     vector<int> result(n+1,1);
-     for(long long i=1;i<n;i++){
-         result[i]=result[i-1]*(n-i+1)/i;
-     }
-     return result;
+    vector<int> getRow(int row) {
+       long long ans = 1;
+    vector<int> ansRow;
+    ansRow.push_back(1); //inserting the 1st element
+
+    //calculate the rest of the elements:
+    for (long long col = 1; col < row+1; col++) {
+        ans = ans * (row+1 - col);
+        ans = ans / col;
+        ansRow.push_back(ans);
+    }
+    return ansRow;
     }
 };
