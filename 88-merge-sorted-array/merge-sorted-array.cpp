9668763713ba6ba62250df2fcd43b2af2455984c
anannyashree sharma <64601048+ananyashreesharma@users.noreply.github.com>
2024-05-01 03:10:45 +0530

    Time: 0 ms (100.00%) | Memory: 11.1 MB (31.19%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -1,49 +1,24 @@
 class Solution {
 public:
     void merge(vector<int>& arr1, int n, vector<int>& arr2, int m) {
-        // int last=arr1.size()-1;
-        //     if(m!=0 and n==0)
-        // {  
-        //  for(int i = 0; i < n; i++){
-        //         arr1[i] = arr2[i];
-        //     } return;
-        // } 
-        //   while(n>0 and m>0){
-        //     if(arr1[n-1]>=arr2[m-1]){
-        //         arr1[last]=arr1[n-1];
-        //         n--;
-        //     }
-        //     else if(arr1[n-1]<=arr2[m-1]){
-        //         arr1[last]=arr2[m-1];
-        //         m--;
-        //     }
-        //     last--;
-        //   }
-         if (n == 0) {
-        // If arr1 is empty, directly copy elements from arr2 to arr1
-        for (int i = 0; i < m; i++) {
-            arr1[i] = arr2[i];
+        int last = arr1.size()-1;
+        while( n>0 and m>0){
+            if(arr1[n-1]>arr2[m-1]){
+                arr1[last]=arr1[n-1];
+                n--;
+            }
+           else  if(arr1[n-1]<=arr2[m-1]){
+                arr1[last]=arr2[m-1];
+                m--;
+            }
+            last--;
         }
-        return;
-    }
-    
-    int last = arr1.size() - 1;
-    while (n > 0 && m > 0) {
-        if (arr1[n - 1] >= arr2[m - 1]) {
-            arr1[last] = arr1[n - 1];
-            n--;
-        } else {
-            arr1[last] = arr2[m - 1];
-            m--;
-        }
-        last--;
-    }
-    
-    // If elements in arr2 are remaining, copy them to arr1
-    while (m > 0) {
-        arr1[last] = arr2[m - 1];
-        m--;
-        last--;
-    }   
+        
+            while(m>0){
+                arr1[last]=arr2[m-1];
+                last--;
+                m--;
+            }
+        
     }
 };
