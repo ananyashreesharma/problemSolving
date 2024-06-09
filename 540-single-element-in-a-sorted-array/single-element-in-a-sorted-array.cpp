13191e29fb773317079fc5d56f6eff8fd001fb07
anannyashree sharma <64601048+ananyashreesharma@users.noreply.github.com>
2024-06-10 00:14:21 +0530

    Time: 24 ms (14.47%) | Memory: 24.6 MB (85.49%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -1,45 +1,34 @@
 class Solution {
 public:
-    int singleNonDuplicate(vector<int>& arr) {
-  int n=arr.size();
-        int ans=0;
-        if(n==1){
-            return arr[0]; 
-        }
-        if(arr[0]!=arr[1]){
-            return arr[0];
+    int singleNonDuplicate(vector<int>& nums) {
+       int n=nums.size();
+       if(n==1){
+        return nums[0];
+       }
 
-        }
-        if(arr[n-1]!=arr[n-2]){
-            return arr[n-1];
-            
-        }
-        int l=1;
-        int h=n-2;
-        while(l<=h){
-            int mid=l+(h-l)/2;
-            if(arr[mid]!=arr[mid+1] and arr[mid]!=arr[mid-1]){
-                return arr[mid];
-            }
-            if((arr[mid]==arr[mid-1] and mid%2!=0) or (arr[mid]==arr[mid+1] and mid%2==0))
-            {
-                l=mid+1;
-                
-            }
-            else 
-            {
-               h=mid-1;
-                
-                
-            }
-            // else {
-            //     return arr[mid];
-            // }
-       
 
-            
+       if(nums[0]!=nums[1]){
+        return nums[0];
+       }
+       if(nums[n-1]!=nums[n-2]){
+        return nums[n-1];
+       }
+      int h=n-2;
+      int l=1;
+      while(l<=h){
+        int mid=l+(h-l)/2;
+        if(nums[mid]!=nums[mid+1] and nums[mid]!=nums[mid-1]){
+            return nums[mid];
         }
-        
-        return -1;
+       else if((nums[mid] == nums[mid-1] && mid % 2 != 0) || (nums[mid] == nums[mid+1] && mid % 2 == 0))
+{
+    l = mid + 1;
+}
+        else {
+            h=mid-1;
+        }
+
+      }
+      return -1;
     }
 };
