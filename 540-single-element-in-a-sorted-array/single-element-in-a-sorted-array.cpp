3ea618cab01393157f3caa17c751b16ba71a084a
anannyashree sharma <64601048+ananyashreesharma@users.noreply.github.com>
2024-06-11 00:40:48 +0530

    Time: 18 ms (65.42%) | Memory: 24.7 MB (51.07%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -2,19 +2,21 @@ class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
        int n=nums.size();
+       // we will follow the odd even sequence in this question
+
        if(n==1){
         return nums[0];
        }
 
 
-       if(nums[0]!=nums[1]){
-        return nums[0];
-       }
-       if(nums[n-1]!=nums[n-2]){
-        return nums[n-1];
-       }
-      int h=n-2;
-      int l=1;
+    //    if(nums[0]!=nums[1]){
+    //     return nums[0];
+    //    }
+    //    if(nums[n-1]!=nums[n-2]){
+    //     return nums[n-1];
+    //    }
+      int h=n-1;
+      int l=0;
       while(l<=h){
         int mid=l+(h-l)/2;
         if(nums[mid]!=nums[mid+1] and nums[mid]!=nums[mid-1]){
@@ -27,7 +29,12 @@ public:
         else {
             h=mid-1;
         }
-
+           if(nums[0]!=nums[1]){
+        return nums[0];
+       }
+       if(nums[n-1]!=nums[n-2]){
+        return nums[n-1];
+       }
       }
       return -1;
     }
