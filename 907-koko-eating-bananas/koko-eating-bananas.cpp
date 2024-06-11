f06960bbe3c940119d57f430c2e192a598adb3ba
anannyashree sharma <64601048+ananyashreesharma@users.noreply.github.com>
2024-06-12 02:36:04 +0530

    Time: 31 ms (63.25%) | Memory: 22 MB (15.08%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -1,9 +1,8 @@
 class Solution {
 public:
- int findMax(vector<int> &v) {
+    int findMax(vector<int> &v) {
     int maxi = INT_MIN;
     int n = v.size();
-    //find the maximum:
     for (int i = 0; i < n; i++) {
         maxi = max(maxi, v[i]);
     }
@@ -13,27 +12,27 @@ public:
 double calculateTotalHours(vector<int> &v, int hourly) {
     double totalH = 0;
     double n = v.size();
-    //find total hours:
     for (int i = 0; i < n; i++) {
         totalH += ceil((double)(v[i]) / (double)(hourly));
     }
     return totalH;
 }
 
-int minEatingSpeed(vector<int> v, int h) {
-    int low = 1, high = findMax(v);
+int minEatingSpeed(std::vector<int> v, int h) {
+    int low = 1, high = findMax(v); // Initialize low to 1 and high to the maximum value in the vector
 
-    //apply binary search:
+    // Binary search for the minimum eating speed
     while (low <= high) {
-        double mid = (low + high) / 2;
-        double totalH = calculateTotalHours(v, mid);
+        double mid = (low + high) / 2; // Calculate the mid-point speed
+        double totalH = calculateTotalHours(v, mid); // Calculate total hours needed at this speed
+        
         if (totalH <= h) {
-            high = mid - 1;
-        }
-        else {
-            low = mid + 1;
+            high = mid - 1; // If total hours is less than or equal to h, search the lower half
+        } else {
+            low = mid + 1; // If total hours is greater than h, search the upper half
         }
     }
-    return low;
+
+    return low; // The lowest speed at which the task can be completed within h hours
 }
 };
