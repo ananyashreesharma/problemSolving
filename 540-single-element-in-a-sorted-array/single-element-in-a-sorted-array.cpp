47f6f6922e16f6e8205ffde4e956bd898037ffc7
anannyashree sharma <64601048+ananyashreesharma@users.noreply.github.com>
2024-09-11 00:44:34 +0530

    Time: 20 ms (43.15%) | Memory: 24.8 MB (84.33%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -1,41 +1,43 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-       int n=nums.size();
-       // we will follow the odd even sequence in this question
-
-       if(n==1){
-        return nums[0];
-       }
-
-
-    //    if(nums[0]!=nums[1]){
-    //     return nums[0];
-    //    }
-    //    if(nums[n-1]!=nums[n-2]){
-    //     return nums[n-1];
-    //    }
-      int h=n-1;
-      int l=0;
-      while(l<=h){
-        int mid=l+(h-l)/2;
-        if(nums[mid]!=nums[mid+1] and nums[mid]!=nums[mid-1]){
-            return nums[mid];
+        int high=nums.size()-2;
+        int low=1;
+        if (nums.size()==1){
+            return nums[0];
         }
-       else if((nums[mid] == nums[mid-1] && mid % 2 != 0) || (nums[mid] == nums[mid+1] && mid % 2 == 0))
-{
-    l = mid + 1;
-}
-        else {
-            h=mid-1;
+        if(nums[0]!=nums[1]){
+            return nums[0];
         }
-           if(nums[0]!=nums[1]){
-        return nums[0];
-       }
-       if(nums[n-1]!=nums[n-2]){
-        return nums[n-1];
-       }
-      }
-      return -1;
+        if(nums[high+1]!=nums[high]){
+            return nums[high+1];
+        }
+      //  int ans;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(nums[mid]!=nums[mid-1] and nums[mid]!=nums[mid+1] ){
+               return nums[mid];
+
+            }
+            if(mid%2!=0){
+                if(nums[mid]==nums[mid+1]){
+                    high=mid-1;
+                }
+                else {
+                    low=mid+1;
+                }
+            }
+            else{
+                if(nums[mid]==nums[mid+1]){
+                    low=mid+2;
+                }
+                else{
+                    high=mid-1;
+                }
+            }
+            
+        }
+        return -1; 
+       
     }
 };
