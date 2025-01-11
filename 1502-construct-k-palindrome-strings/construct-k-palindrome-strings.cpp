5eda44256a17db4314a0def5a0a3500210715128
anannyashree sharma <64601048+ananyashreesharma@users.noreply.github.com>
2025-01-11 22:59:43 +0530

    Time: 8 ms (47.90%) | Memory: 15 MB (33.92%) - LeetSync

--- 1502-construct-k-palindrome-strings/construct-k-palindrome-strings.cpp
+++ 1502-construct-k-palindrome-strings/construct-k-palindrome-strings.cpp
@@ -1,28 +1,19 @@
 class Solution {
 public:
     bool canConstruct(string s, int k) {
-         // Step 1: Count the frequency of each character using a hashmap
-    unordered_map<char, int> freq;
-    for (char c : s) {
-        freq[c]++;
-    }
-
-    // Step 2: Count the total number of characters with odd frequencies
-    int oddCount = 0;
-    for (auto& entry : freq) {
-        if (entry.second % 2 != 0) { // Check if frequency is odd
-            oddCount++;
+        unordered_map<char,int> quency;
+        for(char i: s){
+            quency[i]++;
         }
-    }
-
-    // Step 3: Check conditions
-    // If the total odd characters exceed k, return false
-    // If k is greater than the string length, return false
-    if (oddCount > k || k > s.length()) {
-        return false;
-    }
-
-    // Otherwise, it is possible to form k palindrome strings
-    return true;
+        int oddy=0;
+        for(auto i: quency){
+            if(i.second%2!=0){
+                oddy=oddy+1;
+            }
+        }
+        if(oddy>k or s.size()<k){
+            return false;
+        }
+        return true;
     }
 };
