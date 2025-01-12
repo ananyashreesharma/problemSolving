b1c2eeef812eb027eb0990a1a862b5cb4d788807
anannyashree sharma <64601048+ananyashreesharma@users.noreply.github.com>
2025-01-13 01:43:13 +0530

    Time: 16 ms (44.47%) | Memory: 30 MB (80.15%) - LeetSync

--- 2221-check-if-a-parentheses-string-can-be-valid/check-if-a-parentheses-string-can-be-valid.cpp
+++ 2221-check-if-a-parentheses-string-can-be-valid/check-if-a-parentheses-string-can-be-valid.cpp
@@ -1,23 +1,34 @@
 class Solution {
 public:
     bool canBeValid(string s, string locked) {
-    int n = s.size();
-    if (n % 2 != 0) return false;
-    
-    int open_count = 0;
-    for (int i = 0; i < n; i++) {
-        if (s[i] == '(' || locked[i] == '0') open_count++;
-        else open_count--;
-        if (open_count < 0) return false;
-    }
-
-    int close_count = 0;
-    for (int i = n - 1; i >= 0; i--) {
-        if (s[i] == ')' || locked[i] == '0') close_count++;
-        else close_count--;
-        if (close_count < 0) return false;
-    }
-    
-    return true;
+        int n=s.size();
+        if(n%2!=0){
+            return false;
+        }
+        int open_cnt=0;
+        int close_cnt=0;
+        for(int i=0;i<n;i++){
+            if(s[i]=='(' or locked[i]=='0'){
+                open_cnt=open_cnt+1;
+            }
+            else{
+                open_cnt=open_cnt-1;
+            }
+            if(open_cnt<0){
+                return false;
+            }
+        }
+           for(int i=n-1;i>=0;i--){
+            if(s[i]==')' or locked[i]=='0'){
+                close_cnt=close_cnt+1;
+            }
+            else{
+            close_cnt=close_cnt-1;
+            }
+            if(close_cnt<0){
+                return false;
+            }
+        }
+        return true;
     }
 };
