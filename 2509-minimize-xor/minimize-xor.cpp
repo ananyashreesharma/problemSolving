f4d270fe6527988304fcad1fe97d845150c5f89c
anannyashree sharma <64601048+ananyashreesharma@users.noreply.github.com>
2025-01-15 22:42:44 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (40.19%) - LeetSync

--- 2509-minimize-xor/minimize-xor.cpp
+++ 2509-minimize-xor/minimize-xor.cpp
@@ -3,19 +3,16 @@ public:
   int minimizeXor(int num1, int num2) {
     int numBits = __builtin_popcount(num2);
     int result = 0;
-    
-    for (int i = 31; i >= 0 && numBits > 0; --i) {
-        if ((num1 & (1 << i)) != 0) {
+     for (int i = 31; i >= 0 && numBits > 0; i--) {
+    if ((num1 & (1 << i)) != 0) {
             result |= (1 << i);
-            --numBits;
+            numBits--;
         }
+    } for (int i = 0; i <= 31 && numBits > 0; i++) {
+     if ((result & (1 << i)) == 0) {
+            result |= (1 << i);
+            numBits--;
     }
-    
-    for (int i = 0; i <= 31 && numBits > 0; ++i) {
-        if ((result & (1 << i)) == 0) {
-            result |= (1 << i);
-            --numBits;
-        }
     }
     
     return result;
